aedaef997db70ad85d016c5a980f44c20d1876fd
BARRANGER <barrangernoe@gmail.com>
2024-12-02 23:35:53 +0000

    Update Circle.java PROTO1

--- curlinggame/Circle.java
+++ curlinggame/Circle.java
@@ -3,27 +3,24 @@ package testopenCV;
 import org.opencv.core.Point;
 
 public class Circle {
-    private Point center;
-    private int radius;
+	private Point center;
+	private int radius;
 
-    // Constructor
-    public Circle(Point center, int radius) {
-        this.center = center;
-        this.radius = radius;
-    }
+	public Circle(Point center, int radius) {
+		this.center = center;
+		this.radius = radius;
+	}
 
-    // Getters
-    public Point getCenter() {
-        return center;
-    }
+	public Point getCenter() {
+		return center;
+	}
 
-    public int getRadius() {
-        return radius;
-    }
+	public int getRadius() {
+		return radius;
+	}
 
-    // toString method for debugging purposes
-    @Override
-    public String toString() {
-        return "Circle{Center: " + center + ", Radius: " + radius + "}";
-    }
+	@Override
+	public String toString() {
+		return "Circle{Center: " + center + ", Radius: " + radius + "}";
+	}
 }
