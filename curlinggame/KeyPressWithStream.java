0227dfe0c2f69aa64364c70559c52996d516d50d
BARRANGER <barrangernoe@gmail.com>
2024-12-02 21:06:52 +0000

    Update file KeyPressWithStream.java

--- curlinggame/KeyPressWithStream.java
+++ curlinggame/KeyPressWithStream.java
@@ -8,7 +8,7 @@ import org.opencv.highgui.HighGui;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.videoio.VideoCapture;
 
-public class KeyPressWithStream {
+public class Main {
     static { System.loadLibrary(Core.NATIVE_LIBRARY_NAME); }
 
     public static void main(String[] args) {
@@ -24,34 +24,32 @@ public class KeyPressWithStream {
         Mat background = frame;
         TokenDetector tkd = new TokenDetector(background, List.of(0, 40));  
 
-        int frameCount = 0;
         int key = -1;
 
         while (key != 'Q') {
+            HighGui.imshow("Webcam Stream", frame);
+
             if (!capture.read(frame)) {
                 System.out.println("Error: Could not read frame from stream.");
                 break;
             }
-
-            Circle circle = tkd.detectToken(frame);
-            if (circle != null) {
-                frame = tkd.displayCircles(frame, List.of(circle.getCenter()), List.of(circle.getRadius()), List.of(true));
-                HighGui.imshow("Webcam Stream", frame);
-                
-            } else {
-                HighGui.imshow("Webcam Stream", frame);
-            }
+            //frame = tkd.detectToken(frame);
 
             key = HighGui.waitKey(10);
             if (key != -1) {
                 char pressedKey = (char) key;
-                System.out.println("Key pressed: " + pressedKey);
+                System.out.println("Key: " + pressedKey);
 
                 if (pressedKey == 'F' || pressedKey == 'f') {
                     tkd.setBackground(frame);
                 }
+                if (pressedKey == 'T' || pressedKey == 't') {
+                    frame = tkd.detectToken(frame);
+                }
+                
             }
         }
     }
 
 }
+
